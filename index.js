220660f6cc18f5511db41ff3a73eb9a54c18d051
Jackeline2016 <jack.jimenezalva@gmail.com>
2016-11-19 13:36:49 -0600

    Primer deployment de la aplicacion

---
+++ index.js
@@ -0,0 +1,17 @@
+var express=require('express');
+var app=express();
+var htpp=required('http').server(app);
+//usando socket io
+var io=required('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));//aqui se serviran archivos estaticos
+
+var port=process.env.port || 3000; //ESTO LO EXIGE HEROKU
+app.get('/',function(req,res){
+	res.sendFile(__dirname + '/index.html');//ruta para index
+});
+
+http.listen(port,function(){
+	console.log('Escuchando en el puerto: '+port);
+});
+
